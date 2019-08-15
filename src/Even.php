67be22037af2d8a3a1b1f22e8f58936cc3762309
Alex <vasilev56rus@gmail.com>
2019-08-15 12:29:07 +0300

    add random numers

--- src/Even.php
+++ src/Even.php
@@ -12,32 +12,78 @@ function runGame()
     print_r(PHP_EOL);
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    line('Question: 15');
+    $number1 = rand(1,99);
+    line('Question: %s', $number1);
     $question1 = prompt('Your answer');
-    if ($question1 !== 'no') {
-        line("'%s' is wrong answer ;(. Correct answer was 'no'.", $question1);
-        line("Let's try again, %s!", $name);
-        die();
+    if ($number1 % 2 === 0) {
+        switch ($question1) {
+            case 'yes':
+                line('Correct!');
+                break;
+            case 'no':
+                line("'%s' is wrong answer ;(. Correct answer was 'yes'.", $question1);
+                line("Let's try again, %s!", $name);
+                die();
+        }
     } else {
-        line('Correct!');
+        switch ($question1) {
+            case 'no':
+                line('Correct!');
+                break;
+            case 'yes':
+                line("'%s' is wrong answer ;(. Correct answer was 'no'.", $question1);
+                line("Let's try again, %s!", $name);
+                die();
+        }
     }
-    line('Question: 6');
+    $number2 = rand(1,99);
+    line('Question: %s', $number2);
     $question2 = prompt('Your answer');
-    if ($question2 !== 'yes') {
-        line("'%s' is wrong answer ;(. Correct answer was 'yes'.", $question2);
-        line("Let's try again, %s!", $name);
-        die();
+    if ($number2 % 2 === 0) {
+        switch ($question2) {
+            case 'yes':
+                line('Correct!');
+                break;
+            case 'no':
+                line("'%s' is wrong answer ;(. Correct answer was 'yes'.", $question2);
+                line("Let's try again, %s!", $name);
+                die();
+        }
     } else {
-        line('Correct!');
+        switch ($question2) {
+            case 'no':
+                line('Correct!');
+                break;
+            case 'yes':
+                line("'%s' is wrong answer ;(. Correct answer was 'no'.", $question2);
+                line("Let's try again, %s!", $name);
+                die();
+        }
     }
-    line('Question: 7');
+    $number3 = rand(1,99);
+    line('Question: %s', $number3);
     $question3 = prompt('Your answer');
-    if ($question3 !== 'no') {
-        line("'%s' is wrong answer ;(. Correct answer was 'no'.", $question3);
-        line("Let's try again, %s!", $name);
-        die();
+    if ($number3 % 2 === 0) {
+        switch ($question3) {
+            case 'yes':
+                line('Correct!');
+                line('Congratulations, %s!', $name);
+                break;
+            case 'no':
+                line("'%s' is wrong answer ;(. Correct answer was 'yes'.", $question3);
+                line("Let's try again, %s!", $name);
+                die();
+        }
     } else {
-        line('Correct!');
-        line("Congratulations, %s!", $name);
+        switch ($question3) {
+            case 'no':
+                line('Correct!');
+                line('Congratulations, %s!', $name);
+                break;
+            case 'yes':
+                line("'%s' is wrong answer ;(. Correct answer was 'no'.", $question3);
+                line("Let's try again, %s!", $name);
+                die();
+        }
     }
 }
